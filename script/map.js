a81c853412c7502f6f4e18341358472467bfe43c
xjr83 <98616743+xjr83@users.noreply.github.com>
2022-01-28 21:24:20 +0100

    Initial commit

---
+++ script/map.js
@@ -0,0 +1,158 @@
+﻿function initialize() {
+	var mapCanvas = document.getElementById('map-canvas');
+	var pos = new google.maps.LatLng(54.519511, 18.536473);
+
+	var styles = [
+{
+	"featureType": "administrative",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "off"
+		}
+	]
+},
+{
+	"featureType": "poi",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "simplified"
+		}
+	]
+},
+{
+	"featureType": "road",
+	"elementType": "labels",
+	"stylers": [
+		{
+			"visibility": "simplified"
+		}
+	]
+},
+{
+	"featureType": "water",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "simplified"
+		}
+	]
+},
+{
+	"featureType": "transit",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "simplified"
+		}
+	]
+},
+{
+	"featureType": "landscape",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "simplified"
+		}
+	]
+},
+{
+	"featureType": "road.highway",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "off"
+		}
+	]
+},
+{
+	"featureType": "road.local",
+	"elementType": "all",
+	"stylers": [
+		{
+			"visibility": "on"
+		}
+	]
+},
+{
+	"featureType": "road.highway",
+	"elementType": "geometry",
+	"stylers": [
+		{
+			"visibility": "on"
+		}
+	]
+},
+{
+	"featureType": "water",
+	"elementType": "all",
+	"stylers": [
+		{
+			"color": "#84afa3"
+		},
+		{
+			"lightness": 52
+		}
+	]
+},
+{
+	"featureType": "all",
+	"elementType": "all",
+	"stylers": [
+		{
+			"saturation": -17
+		},
+		{
+			"gamma": 0.36
+		}
+	]
+},
+{
+	"featureType": "transit.line",
+	"elementType": "geometry",
+	"stylers": [
+		{
+			"color": "#3f518c"
+		}
+	]
+}
+	];
+
+	var mapOptions = {
+		center: pos,
+		zoom: 18,
+		scrollwheel: false,
+		navigationControl: false,
+		mapTypeControl: false,
+		scaleControl: false,
+		draggable: true,
+		disableDoubleClickZoom: true,
+		disableDefaultUI: true,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: styles,
+	}
+
+	var map = new google.maps.Map(mapCanvas, mapOptions);
+	var marker = new google.maps.Marker({
+		position: pos,
+		map: map,
+		title: 'Krzysztof Maj - Kancelaria Adwokacka',
+		icon: '/images/map.png'
+	});
+
+	var desc =
+		'<div class="content text-center" style="margin: 16px 0 0 16px">' +
+			'<img src="/images/logo.svg" class="logo-small" alt="Kancelaria" />' +
+		'</div>';
+
+	var infowindow = new google.maps.InfoWindow({
+		content: desc
+	});
+
+	google.maps.event.addListener(marker, 'click', function () {
+		infowindow.open(map, marker);
+	});
+	infowindow.open(map, marker);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ script/scroll.js
@@ -0,0 +1,20 @@
+﻿/* Enable smooth scrolling on all links with anchors */
+$('.navbar-right li a[href^="#"]').on('click', function (e) {
+
+	// prevent default anchor click behavior
+	e.preventDefault();
+
+	// store hash
+	var hash = this.hash;
+
+	// animate
+	$('html, body').animate({
+		scrollTop: $('section[id="' + this.hash.replace('#', '') + '"]').offset().top - 150
+	}, 300, function () {
+
+		// when done, add hash to url
+		// (default click behaviour)
+		window.location.hash = hash;
+
+	});
+});
